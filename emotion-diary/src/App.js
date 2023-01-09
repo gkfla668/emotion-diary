fb4e19eb9beea35bb09cfd952ab598f1a1d6cd0e
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2023-01-09 16:34:09 +0900

    [feat]: 상태 로직 분리하기 (useReducer), dispatch 함수 생성 - onCreate - onRemove - onEdit

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -1,11 +1,82 @@
+import { useReducer, useRef } from "react";
+
 import "./App.css";
 import MyButton from "./components/MyButton";
 import MyHeader from "./components/MyHeader";
 
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Edit from "./pages/Edit";
+import Diary from "./pages/Diary";
+
+const reducer = (state, action) => {
+  let newState = [];
+
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      newState = [action.data, ...state];
+      break;
+    }
+    case "REMOVE": {
+      newState = state.filter((it) => it.id !== action.targetId);
+      break;
+    }
+    case "EDIT": {
+      newState = state.map((it) =>
+        it.id === action.targetId ? { ...action.data } : it
+      );
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+};
+
 function App() {
+  const [data, dispatch] = useReducer(reducer, []);
+  const dataId = useRef(0);
+
+  const onCreate = (date, content, emotion) => {
+    dispatch({
+      type: "CREAT",
+      data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+
+    dataId.current += 1;
+  };
+
+  const onRemove = (targetId) => {
+    dispatch({ type: "REMOVE", targetId });
+  };
+
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: "EDIT",
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+  };
+
   return (
     <div className="App">
-      <MyHeader leftChild={<MyButton text={"왼쪽"}></MyButton>} head_text={"일기장"} rightChild={<MyButton text={"오른쪽"}></MyButton>}></MyHeader>
+      <MyHeader
+        leftChild={<MyButton text={"왼쪽"}></MyButton>}
+        head_text={"일기장"}
+        rightChild={<MyButton text={"오른쪽"}></MyButton>}
+      ></MyHeader>
     </div>
   );
 }
