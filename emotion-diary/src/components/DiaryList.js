77f93d00769599dc16da589e8213ce25413b782e
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2023-01-09 17:58:21 +0900

    [feat]: 일기 리스트 날짜 순 정렬 option 생성

--- emotion-diary/src/components/DiaryList.js
+++ emotion-diary/src/components/DiaryList.js
@@ -1,6 +1,29 @@
+import { useState } from "react";
+
+const sortOptionList = [
+  { value: "latest", name: "최신순" },
+  { value: "oldest", name: "오래된순" },
+];
+
+const ControlMenu = ({ value, onChange, optionList }) => {
+  return (
+    <select value={value} onChange={(e) => onChange(e.target.value)}>
+      {optionList.map((it) => (
+        <option value={it.value}>{it.name}</option>
+      ))}
+    </select>
+  );
+};
 const DiaryList = ({ diaryList }) => {
+  const [sortType, setSortType] = useState("latest");
+
   return (
     <div className="DiaryList">
+      <ControlMenu
+        value={sortType}
+        onChange={setSortType}
+        optionList={sortOptionList}
+      ></ControlMenu>
       {diaryList.map((it) => (
         <div ket={it.id}>{it.content}</div>
       ))}
