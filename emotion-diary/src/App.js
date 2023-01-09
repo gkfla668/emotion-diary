b4483cde86260d128e8b3f6169cde41914939c2c
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2023-01-09 15:28:05 +0900

    [design]: 일기장 size

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import MyButton from "./components/MyButton";
 
 function App() {
   return (
     <div className="App">
-     
+      <h2>App.js</h2>
     </div>
   );
 }
