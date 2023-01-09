6de9102401f5b1734400619de62093723ba44f3c
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2023-01-09 17:11:20 +0900

    [feat]: 날짜 이동 버튼 함수 생성 - increaseMonth - decreaseMonth

--- emotion-diary/src/pages/Home.js
+++ emotion-diary/src/pages/Home.js
@@ -6,12 +6,24 @@ const Home = () => {
   const [curDate, setCurDate] = useState(new Date());
   const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
 
+  const increaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
+    );
+  };
+
+  const decreaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+    );
+  };
+
   return (
     <div>
       <MyHeader
-        leftChild={<MyButton text={"<"}></MyButton>}
+        leftChild={<MyButton text={"<"} onClick={decreaseMonth}></MyButton>}
         head_text={headText}
-        rightChild={<MyButton text={">"}></MyButton>}
+        rightChild={<MyButton text={">"} onClick={increaseMonth}></MyButton>}
       ></MyHeader>
     </div>
   );
