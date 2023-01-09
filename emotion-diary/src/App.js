aee579f67e203eaec7664cd5fb18e95cd90f5cb6
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2023-01-09 15:42:59 +0900

    [feat]: MyHeader Component

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -1,14 +1,11 @@
 import "./App.css";
 import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
 
 function App() {
   return (
     <div className="App">
-      <h2>App.js</h2>
-      <MyButton
-        text={"뒤로가기"}
-        onClick={() => alert("뒤로가기 버튼 클릭!")}
-      ></MyButton>
+      <MyHeader leftChild={<MyButton text={"왼쪽"}></MyButton>} head_text={"일기장"} rightChild={<MyButton text={"오른쪽"}></MyButton>}></MyHeader>
     </div>
   );
 }
---
+++ emotion-diary/src/components/MyHeader.js
@@ -0,0 +1,11 @@
+const MyHeader = ({ leftChild, head_text, rightChild }) => {
+  return (
+    <header>
+      <div className="head_btn_left">{leftChild}</div>
+      <div className="head_text">{head_text}</div>
+      <div className="head_btn_right">{rightChild}</div>
+    </header>
+  );
+};
+
+export default MyHeader;
