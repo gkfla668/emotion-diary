7359a9e78a301169eb3ca1020782c68b4fc86f72
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2023-01-09 19:52:42 +0900

    [feat]: 일기 리스트 감정 필터링 option 생성

--- emotion-diary/src/components/DiaryList.js
+++ emotion-diary/src/components/DiaryList.js
@@ -5,6 +5,12 @@ const sortOptionList = [
   { value: "oldest", name: "오래된순" },
 ];
 
+const filterOptionList = [
+  { value: "all", name: "모두" },
+  { value: "good", name: "좋은 감정만" },
+  { value: "bad", name: "나쁜 감정만" },
+];
+
 const ControlMenu = ({ value, onChange, optionList }) => {
   return (
     <select value={value} onChange={(e) => onChange(e.target.value)}>
@@ -14,8 +20,38 @@ const ControlMenu = ({ value, onChange, optionList }) => {
     </select>
   );
 };
+
 const DiaryList = ({ diaryList }) => {
   const [sortType, setSortType] = useState("latest");
+  const [filter, setFilter] = useState("all");
+
+  const getProcessedDiaryList = () => {
+    const filterCallBack = (item) => {
+      if (filter === "good") {
+        return parseInt(item.emotion) <= 3;
+      }
+
+      return parseInt(item.emotion) > 3;
+    };
+
+    const compare = (a, b) => {
+      if (sortType === "latest") {
+        return parseInt(b.date) - parseInt(a.date);
+      }
+
+      return parseInt(a.date) - parseInt(b.date);
+    };
+
+    // 원본 배열 건드리지 않기 위해 깊은 복사
+    const copyList = JSON.parse(JSON.stringify(diaryList)); // 문자열로 바꼈다가 배열로
+
+    const filterList =
+      filter === "all" ? copyList : copyList.filter((it) => filterCallBack(it));
+
+    const sortedList = filterList.sort(compare);
+
+    return sortedList;
+  };
 
   return (
     <div className="DiaryList">
@@ -24,7 +60,12 @@ const DiaryList = ({ diaryList }) => {
         onChange={setSortType}
         optionList={sortOptionList}
       ></ControlMenu>
-      {diaryList.map((it) => (
+      <ControlMenu
+        value={filter}
+        onChange={setFilter}
+        optionList={filterOptionList}
+      ></ControlMenu>
+      {getProcessedDiaryList().map((it) => (
         <div ket={it.id}>{it.content}</div>
       ))}
     </div>
